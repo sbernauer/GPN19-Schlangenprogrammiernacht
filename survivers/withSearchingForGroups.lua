d7da088e8f68754ec602cdf7d6de80e6ee32eb42
Sebastian Bernauer <sbernauer@united-internet.de>
2019-05-15 16:17:49 +0200

    Pretty good

--- survivers/withSearchingForGroups.lua
+++ survivers/withSearchingForGroups.lua
@@ -1,42 +1,50 @@
 --- SURVIVER WITH CLUSTER SEARCH!!!
 function init()
     self.colors = { 0xFF0000, 0xFF7F00, 0xFFFF0, 0x00FF00, 0x0000FF, 0x4B0082, 0x9400D3 }
-    maxFoodSeen = 0
+    rightAngleCounter = 0
+    circleEscaperSteps = 0
 end
 
 function step()
-    -- log(string.format("My segment_radius: %.4f sight_radius: %.4f consume_radius: %.4f", self.segment_radius, self.sight_radius, self.consume_radius))
+
+    if (circleEscaperSteps > 0) then
+        circleEscaperSteps = circleEscaperSteps - 1
+        return 0
+    end
 
     local food = findFood(self.sight_radius, 0.8)
 
     local movingX = 0
     local movingY = 0
 
-    -- log(string.format("Found %d food with sight %f. max: %f", #food, self.sight_radius, maxFoodSeen))
-    -- If there is not enoguh food go where else
-    maxFoodSeen = math.max(maxFoodSeen, #food)
-    -- if((#food < 10 and #food <= (0.4 * maxFoodSeen - 1)) or (maxFoodSeen < 10 and #food <= 1)) then
-    if (#findFood(60, 0.8) <= 1)
-        log(string.format("Runnaway when seen %d food, maxfood: %d", #food, maxFoodSeen))
-        return 0
-    end
+    -- if (#food <= 1) then
+    --     log(string.format("Runnaway, found only %d food", #food))
+    --     return -food[1].d -- Array starts at 1 :(
+    -- end
 
     for i, item in pairs(food) do
+
         local distance = item.dist
         local direction = item.d
         local mass = item.v
 
-        local directionCost = math.abs(direction)
-        local distanceCost = 1 / distance
-
-        -- log(string.format("distancecost %f direction %f", distanceCost, directionCost))
-
-        local totalCost = mass * distanceCost * directionCost
-
-        movingX = movingX + (math.sin(direction) * totalCost)
-        movingY = movingY + (math.cos(direction) * totalCost)
+        movingX = movingX + (math.sin(direction) * (1 / distance) * mass)
+        movingY = movingY + (math.cos(direction) * (1 / distance) * mass)
     end
     local finalRotation = math.atan(movingX, movingY)
+
+    
+    -- If running in circle
+    if (math.abs(finalRotation) >= 1.6) then
+        -- log(string.format("finalRotation: %f", finalRotation))
+        rightAngleCounter = rightAngleCounter + 1
+        if (rightAngleCounter >= 20) then
+            log("Loop detected")
+            circleEscaperSteps = 50
+        end
+    else
+        rightAngleCounter = 0
+    end
 
     return finalRotation
 end
