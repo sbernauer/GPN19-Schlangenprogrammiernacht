99949da17a6c58b4b6d886488bf275cf0540e52a
Sebastian Bernauer <sbernauer@united-internet.de>
2019-05-23 21:38:57 +0200

    Added avoidance of enemies

--- survivers/withSearchingForGroups.lua
+++ survivers/withSearchingForGroups.lua
@@ -3,6 +3,10 @@ function init()
     self.colors = { 0xFF0000, 0xFF7F00, 0xFFFF0, 0x00FF00, 0x0000FF, 0x4B0082, 0x9400D3 }
     rightAngleCounter = 0
     circleEscaperSteps = 0
+
+    logCounter = 0
+
+    ENEMY_FACTOR = 1000
 end
 
 function step()
@@ -12,10 +16,10 @@ function step()
         return 0
     end
 
-    local food = findFood(self.sight_radius, 0.8)
-
     local movingX = 0
     local movingY = 0
+
+    local food = findFood(self.sight_radius, 0)
 
     -- if (#food <= 1) then
     --     log(string.format("Runnaway, found only %d food", #food))
@@ -23,7 +27,6 @@ function step()
     -- end
 
     for i, item in pairs(food) do
-
         local distance = item.dist
         local direction = item.d
         local mass = item.v
@@ -31,20 +34,48 @@ function step()
         movingX = movingX + (math.sin(direction) * (1 / distance) * mass)
         movingY = movingY + (math.cos(direction) * (1 / distance) * mass)
     end
-    local finalRotation = math.atan(movingX, movingY)
 
+    local segments = findSegments(self.sight_radius, false)
+
+    -- If there are enemies nearby dont look for food
+    -- if(#segments > 0) then
+        -- movingX = 0
+        -- movingY = 0
+    -- end
+
+    for i, item in pairs(segments) do
+        -- log_frequent(string.format("Segment: distance: %f direction: %f name: %s", item.dist, item.d, item.bot_name))
+        local distance = item.dist
+        local direction = item.d
+        local mass = item.r
+        local name = item.bot_name
+
+        -- if (name NOT UNSERE_EIGENEN_SCHLANGEN)
+        movingX = movingX - ENEMY_FACTOR * (math.sin(direction) * (1 / (distance * distance)) * mass)
+        movingY = movingY - ENEMY_FACTOR * (math.cos(direction) * (1 / (distance * distance)) * mass)
+    end
+    log_frequent(string.format("Anzahl: %d", #segments))
+    local finalRotation = math.atan(movingX, movingY)
     
     -- If running in circle
-    if (math.abs(finalRotation) >= 1.6) then
+    if (math.abs(finalRotation) >= 1.55) then
         -- log(string.format("finalRotation: %f", finalRotation))
         rightAngleCounter = rightAngleCounter + 1
-        if (rightAngleCounter >= 20) then
+        if (rightAngleCounter >= 25) then
             log("Loop detected")
-            circleEscaperSteps = 50
+            circleEscaperSteps = 40
         end
     else
         rightAngleCounter = 0
     end
 
     return finalRotation
+end
+
+function log_frequent(message)
+    logCounter = logCounter + 1
+    if (logCounter >= 10) then
+        logCounter = 0
+        log(message)
+    end
 end
--- utils/registerUsers_WIP.sh
+++ utils/registerUsers_WIP.sh
@@ -2,18 +2,7 @@
 
 HOST="localhost"
 
-CODE=$(cat << EOF
---- MY CODE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-function init()
-    self.colors = { 0xFF0000, 0x808080 }
-end
-
-function step()
-    return -0.05
-end
-
-EOF
-)
+CODE=$(cat ../survivers/withSearchingForGroups.lua)
 
 registerUser() {
     username=$1
@@ -35,7 +24,7 @@ registerUser() {
     html=$(curl -s -b cookie.txt --cookie-jar cookie.txt "$HOST/snake/edit/latest")
     csrfMiddlewareToken=$(echo $html | grep -oP "'csrfmiddlewaretoken' value='\K[a-zA-Z0-9]*")
 
-    curl -s -X POST -b cookie.txt --header "X-CSRFToken: $csrfMiddlewareToken" --header "X-Requested-With: XMLHttpRequest" -F "action=run" -F "code=$CODE" -F "comment=null" -F "parent=6" "localhost/snake/edit/latest" --trace-ascii /dev/stdout # > /dev/null
+    curl -s -X POST -b cookie.txt --header "X-CSRFToken: $csrfMiddlewareToken" --header "X-Requested-With: XMLHttpRequest" -F "action=run" -F "code=$CODE" -F "comment=null" -F "parent=null" "localhost/snake/edit/latest" > /dev/null # --trace-ascii /dev/stdout # > /dev/null
     #curl -v --request POST -b cookie.txt --header "X-CSRFToken: $csrfMiddlewareToken" --header "X-Requested-With: XMLHttpRequest" --data '{"action":"run","code":"'"$CODE"'", "comment":null,"parent":null}' "localhost/snake/edit/latest" --trace-ascii /dev/stdout
     #curl -v -X POST -b cookie.txt --header "X-CSRFToken: $csrfMiddlewareToken" --header "X-Requested-With: XMLHttpRequest" "localhost/snake/restart"
     #curl -v -X POST -b cookie.txt --header "X-CSRFToken: $csrfMiddlewareToken" --header "X-Requested-With: XMLHttpRequest" -F "action=run" -F "code=$CODE" -F "comment=null" -F "parent=null" "localhost/snake/edit/latest"
@@ -43,11 +32,10 @@ registerUser() {
 }
 
 registerAllUsers() {
-    cat usernames/survivers.txt usernames/killers.txt usernames/feeders.txt | while read username
+    cat ../usernames.txt | while read username
     do
         registerUser $username
     done
 }
 
-#registerAllUsers
-registerUser Malte
+registerAllUsers
